63cc5614470ccd7c653d5794179c2cad41edf841
hafis mandala <hfis15@yahoo.co.id>
2021-03-09 16:16:12 +0700

    change setName

--- app/src/main/java/com/hamaar/mysimplecleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/hamaar/mysimplecleanarchitecture/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         val factory = MainViewModelFactory.getInstance()
         val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
-        viewModel.setName("Hafis")
+        viewModel.setName("Hamaar")
         viewModel.message.observe(this, Observer {
             binding.tvWelcome.text = it.welcomeMessage
         })
